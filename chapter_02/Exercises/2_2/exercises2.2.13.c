b440025b4aba0e8c7b833170d0c2e828e181985a
dmego <dmeago@gmail.com>
2024-03-21 19:18:35 +0800

    add chapter_02 exercise 2.2.13

--- chapter_02/Exercises/2_2/exercises2.2.13.c
+++ chapter_02/Exercises/2_2/exercises2.2.13.c
@@ -9,9 +9,51 @@
  */
 #include "SqList.h"
 
-
+/*
+  定义一个长度为 L-length + 1 的数组
+    最小正整数 最小值取 1， 而最大值一定取 L->length + 1 (假设序列中的值从1开始的递增序列)
+ */
+ElemType findMinPositiveNum(SqList *L) {
+    // 定义一个数组，用于存储当前数组下标所代表的正整数在数组中是否有出现
+    int num[L->length + 1];
+    // 初始化数组元素都为 0
+    for (int i = 0; i < L->length + 1; i++) {
+        num[i] = 0;
+    }
+    // 遍历序列，如果序列中的元素在 0 <= e <= L->length 之间，存在到 num 对应下标下
+    for (int i = 0; i < L->length; i++) {
+        if (L->data[i] > 0 && L->data[i] <= L->length) {
+            num[L->data[i]] = 1;
+        }
+    }
+    // 遍历 num 数组，如果对应下标下值不等于 1，则说明这个下标就是最小正整数
+    for (int i = 1; i < L->length + 1; i++) {
+        if (num[i] == 0) {
+            return i;
+        }
+    }
+    // 否则 最小正整数就是最大值 L->length + 1
+    return L->length + 1;
+}
 
 
 int main() {
+
+    SqList L;
+    InitList(&L);
+    ListInsert(&L, 1, -3);
+    ListInsert(&L, 2, 5);
+    ListInsert(&L, 3, -2);
+    ListInsert(&L, 4, 3);
+    ListInsert(&L, 5, -1);
+    ListInsert(&L, 6, 0);
+    ListInsert(&L, 7, 1);
+    ListInsert(&L, 8, 4);
+
+    ElemType e;
+
+    e = findMinPositiveNum(&L);
+
+    printf("min positive num: %d\n", e);
 
 }
