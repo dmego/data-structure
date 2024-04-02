f34bd43b1ae5a5ffc1d4bd0439278d0de62a13d5
dmego <dmeago@gmail.com>
2024-04-02 18:18:13 +0800

    add chapter_02 exercise 2.3.2 2.3.3

--- chapter_02/Exercises/2_3/exercises2.3.2.c
+++ chapter_02/Exercises/2_3/exercises2.3.2.c
@@ -1,11 +1,37 @@
 /*
-
+试编写在带头结点的单链表 L 中删除一个最小值结点的高效算法(假设该结点唯一)
  */
 
+#include "LinkList.h"
 
+void delMinNode(LinkList list) {
+    // head 指针指向头结点
+    LNode *head = list;
+    LNode *minNode = list->next;
+    // dummy 结点指向  的前一个结点
+    LNode *dummy = list;
+    while (head->next != NULL) {
+        if (head->next->data < minNode->data) {
+            minNode = head->next;
+            dummy = head;
+        }
+        head = head->next;
+    }
+    // 删除 minNode 结点
+    dummy->next = minNode->next;
+    free(minNode);
+}
 
 
 
 int main() {
-
+    LinkList list;
+    InitList(&list);
+    int num[] = {6,2,3,4,5};
+    tailCreateListByArray(&list, num, 5);
+    printf("before del:\n");
+    printLinkList(list);
+    delMinNode(list);
+    printf("after del:\n");
+    printLinkList(list);
 }
--- chapter_02/Exercises/2_3/exercises2.3.3.c
+++ chapter_02/Exercises/2_3/exercises2.3.3.c
@@ -1,11 +1,34 @@
 /*
-
+ 试编写算法将带头结点的单表链就地逆置，所谓“就”地是指辅助空间复杂度为O(1)。
  */
+#include "LinkList.h"
 
+void reverseList(LinkList list) {
+    // 头指针指向头结点
+    LNode *head = list;
+    // node 表示要逆置结点的前一个结点 (首元结点不用逆置，node 指向首元结点)
+    LNode *node = list->next;
+    while (node->next != NULL) {
+        // 先将 curr 从链表中断开
+        LNode *curr = node->next;
+        node->next = curr->next;
+        // 再将 curr 插入到最前面
+        curr->next = head->next;
+        head->next = curr;
 
+        // 注意 node 结点经过逆置后，已经处于正确位置 不用执行 node = node->next
 
-
+    }
+}
 
 int main() {
-
+    LinkList list;
+    InitList(&list);
+    int num[] = {1,2,3,4,5};
+    tailCreateListByArray(&list, num, 5);
+    printf("before:\n");
+    printLinkList(list);
+    reverseList(list);
+    printf("after:\n");
+    printLinkList(list);
 }
